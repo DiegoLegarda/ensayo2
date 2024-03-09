097ebe165bd170d226d8bbe65749459c35e799e7
DiegoLegarda <49292082+DiegoLegarda@users.noreply.github.com>
2024-03-08 19:43:14 -0500

    Revert "Se replantea la estructura de los archivos del sitio"

--- js/script.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById("boton").addEventListener("click",escribirTexto); 
-//document.getElementById("myText").addEventListener("input",escribirTexto); 
-
-function escribirTexto() {
-  let x = document.getElementById("myText").value;
-  console.log(x);
-   document.getElementById("demo").innerHTML = x;
-}
